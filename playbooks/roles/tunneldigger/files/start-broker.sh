45aaed4094b601390996488dc783dbebf861e6a3
Christoph Schwadorf <schwadorf@it-service-schwadorf.de>
2019-07-07 21:32:01 +0200

    Korrekturen

---
+++ playbooks/roles/tunneldigger/files/start-broker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+/bin/systemctl reload ferm
+
+WDIR=/srv/tunneldigger/env_tunneldigger
+VIRTUALENV_DIR=/srv/tunneldigger/env_tunneldigger
+
+cd $WDIR
+source $VIRTUALENV_DIR/bin/activate
+
+$VIRTUALENV_DIR/bin/python -m tunneldigger_broker.main  ../l2tp_broker.cfg
+#bin/python broker/l2tp_broker.py ../l2tp_broker.cfg
+
