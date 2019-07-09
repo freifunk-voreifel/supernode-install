b10db4de0d4d0874c0373abf406d5d7e80141656
Christoph Schwadorf <schwadorf@it-service-schwadorf.de>
2019-07-09 14:01:20 +0200

    Tunneldigger Blockliste eingebaut, IPv6 Adresse auf LAN Interface bei lokaler Ausleitung beibehalten

--- playbooks/roles/tunneldigger/files/bataddif.sh
+++ playbooks/roles/tunneldigger/files/bataddif.sh
@@ -3,6 +3,15 @@ INTERFACE="$3"
 MAC="$8"
 brctl=/sbin/brctl
 
+BLOCKLISTE=$(/bin/cat /etc/tunneldigger/blocklist)
+
 /bin/ip link set dev $INTERFACE up mtu 1312
+
+for i in $BLOCKLISTE;
+do
+    if [[ $i == $MAC ]]; then
+      exit 1
+    fi
+done
 
 $brctl addif br-nodes $INTERFACE
